30c5b244f050f8b42812dbf4ee97838509bb496e
Ben <ben10baek@hotmail.com>
2020-12-21 18:46:11 -0800

    display weather and temperature

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-async function weatherData(location, units) {
+const submit = document.getElementById('submitBtn');
+submit.addEventListener('click', submitLocation);
+function submitLocation() {
+	let location = document.getElementById('location').value;
+	let units = document.getElementById('units').value;
+	weatherData(location, units);
+}
+
+async function weatherData(location, unit) {
 	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=31e9c3eab4853f005e67fc274311227e&units=${units}
+		`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=31e9c3eab4853f005e67fc274311227e&units=${unit}
 	`,
 		{
 			mode: 'cors',
@@ -8,15 +16,27 @@ async function weatherData(location, units) {
 	);
 	const data = await response.json();
 	console.log(data);
-	processData(data);
+	const weatherStatus = processData(data);
+	displayWeather(weatherStatus, unit);
 }
 
 function processData(data) {
 	let weather = [];
-	data.weather.forEach((element) => weather.push(element.main));
+	data.weather.forEach((e) => weather.push(e.main));
 	const temp = data.main.temp;
-	let str = weather.join(', ');
-	console.log(`Weather: ${str} \nTemperature: ${temp}`);
+	let strWeather = weather.join(', ');
+	console.log(`Weather: ${strWeather} \nTemperature: ${temp}`);
+	const newData = {
+		weatherState: strWeather,
+		temperature: temp,
+	};
+	return newData;
 }
 
-weatherData('Seattle,washington', 'imperial');
+function displayWeather(processedData, unit) {
+	let weather = processedData.weatherState;
+	let temp = processedData.temperature;
+	unit = unit === 'Imperial' ? '&#8457' : '&#8451';
+	document.getElementById('weather').innerHTML = `Weather: ${weather}`;
+	document.getElementById('temperature').innerHTML = `Temperature: ${temp}${unit}`;
+}
