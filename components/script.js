0ee139d8b9356ae9139c27a977e8826fde0fe20e
Ben <ben10baek@hotmail.com>
2020-12-22 21:38:15 -0800

    Added to style

--- components/script.js
+++ components/script.js
@@ -52,37 +52,46 @@ function processData(data) {
 	return newData;
 }
 
-function displayWeather(processedData, unit) {
-	loader.style.display = 'none';
-	weatherDisplay.style.display = 'block';
-	let weather = processedData.weatherState;
-	let temp = processedData.temperature;
-	let tempHigh = processedData.tempMax;
-	let tempLow = processedData.tempMin;
-	let icon = processedData.icon;
+function weatherIcon(icon) {
 	var iconImage = '';
 	switch (icon) {
 		case '01d':
 			iconImage = 'wi-day-sunny';
 			break;
+		case '01n':
+			iconImage = 'wi-night-clear';
+			break;
 		case '02d':
 			iconImage = 'wi-day-cloudy';
+			break;
+		case '02n':
+			iconImage = 'wi-night-alt-cloudy';
 			break;
 		case '03d':
 		case '03n':
 			iconImage = 'wi-cloud';
 			break;
 		case '04d':
+		case '04n':
 			iconImage = 'wi-cloudy';
 			break;
 		case '09d':
 			iconImage = 'wi-rain';
 			break;
+		case '09n':
+			iconImage = 'wi-night-alt-rain';
+			break;
 		case '10d':
 			iconImage = 'wi-day-rain';
 			break;
+		case '10n':
+			iconImage = 'wi-night-alt-rain';
+			break;
 		case '11d':
 			iconImage = 'wi-thunderstorm';
+			break;
+		case '11n':
+			iconImage = 'wi-night-alt-thunderstorm';
 			break;
 		case '13d':
 		case '13n':
@@ -94,14 +103,43 @@ function displayWeather(processedData, unit) {
 			break;
 	}
 	console.log(`icon image: ${iconImage}`);
+	return iconImage;
+}
+
+function displayWeather(processedData, unit) {
+	loader.style.display = 'none';
+	weatherDisplay.style.display = 'block';
+	let weather = processedData.weatherState;
+	let temp = processedData.temperature;
+	let tempHigh = processedData.tempMax;
+	let tempLow = processedData.tempMin;
+	let icon = processedData.icon;
+	const iconImage = weatherIcon(icon);
+	getTime(icon);
+
 	unit = unit === 'Imperial' ? '&#8457' : '&#8451';
-	var currentWeather = document.getElementById('weather');
+	const currentWeather = document.getElementById('weather');
 	currentWeather.innerHTML = `Weather: ${weather}`;
 	document.getElementById('temperature').innerHTML = `Temperature: ${temp}${unit}`;
 	document.getElementById('highLow').innerHTML = `H: ${tempHigh}${unit} L: ${tempLow}${unit}`;
-
-	var newIcon = document.createElement('i');
+	var currentIcon = document.getElementById('icon');
+	if (currentIcon) {
+		currentIcon.remove();
+	}
+	const newIcon = document.createElement('i');
 	newIcon.id = 'icon';
 	newIcon.className = `wi ${iconImage}`;
 	weatherDisplay.appendChild(newIcon);
+	currentWeather.after(newIcon);
+}
+
+function getTime(icon) {
+	const time = icon.includes('d') ? 'day' : 'night';
+	if (time === 'night') {
+		document.body.style.backgroundColor = 'black';
+		document.getElementById('content').style.color = 'white';
+	} else {
+		document.body.style.backgroundColor = 'white';
+		document.getElementById('content').style.color = 'black';
+	}
 }
